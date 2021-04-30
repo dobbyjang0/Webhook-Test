c0d314864345b8d90420ce1e233748073d85a285
IncheonYSH <ysh121095@gmail.com>
2021-05-01 01:02:33 +0900

    asdfs

--- hook.py
+++ hook.py
@@ -1,16 +1,20 @@
 from github_webhook import Webhook
 from flask import Flask
+from flask import request
+import json
 
 app = Flask(__name__)  # Standard Flask app
 webhook = Webhook(app) # Defines '/postreceive' endpoint
 
-@app.route("/", methods=['POST'])        # Standard Flask endpoint
+@app.route("/")        # Standard Flask endpoint
 def hello_world():
     return "Hello, World!"
 
 
-@app.route("/payload", methods=['POST'])        # Standard Flask endpoint
+@app.route("/payload")        # Standard Flask endpoint
 def sipal():
+    if request.method == 'POST':
+        return "test"
     return "test /payload"
 
 @webhook.hook()        # Defines a handler for the 'push' event
@@ -21,4 +25,4 @@ def on_push(data):
 
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=80)
+    app.run(host="0.0.0.0", port=80)
