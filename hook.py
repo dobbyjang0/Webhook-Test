f8767e200fbea287beaf12450095ee34bca8b735
IncheonYSH <ysh121095@gmail.com>
2021-04-30 22:10:07 +0900

    1eirgh

--- hook.py
+++ hook.py
@@ -4,7 +4,7 @@ from flask import Flask
 app = Flask(__name__)  # Standard Flask app
 webhook = Webhook(app) # Defines '/postreceive' endpoint
 
-@app.route("/")        # Standard Flask endpoint
+@app.route("/", methods=['POST'])        # Standard Flask endpoint
 def hello_world():
     return "Hello, World!"
 
